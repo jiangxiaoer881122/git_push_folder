71f98f1762a259f2dec356ee08b0b4449dd20b17
jjl <757405372@qq.com>
2023-12-14 16:06:16 +0800

    this  can detect timeout and  Ctrl+c 12.14

--- tcp_server.py
+++ tcp_server.py
@@ -16,16 +16,19 @@ server_socket.bind((HOST, PORT))
 server_socket.listen(1)
 print(f"Server started and listening on {HOST}:{PORT}")
 
-# 等待客户端连接
+# 等待客户端连接（阻塞等待）
 client_socket, client_address = server_socket.accept()
 print(f"Client {client_address[0]}:{client_address[1]} connected")
+
+# 设置接收超时时间为5秒（可根据实际情况调整）
+client_socket.settimeout(5)
 
 # 根据当前时间生成文件名
 current_time = time.strftime("%Y%m%d_%H%M%S", time.localtime())
 file_name = f"data_{current_time}.txt"
 file_path = f"D:/pythonwpace/tcp_client/{file_name}"  # 替换为实际文件路径
-try:
-    with open(file_path,"w") as file:
+with open(file_path,"w") as file:
+    try:
         while True:
             try:
                 # 接收客户端数据
@@ -44,11 +47,25 @@ try:
                 #捕捉Ctrl+C的中断信号，保存文件并关闭系统
                 print("Keyboard interrupt detected. Saving file and exiting...")
                 break
-except Exception as e:
-    print(f"An error occurred: {e}")
-finally:
-    #关闭客户端连接和服务器套接字
-    # 关闭客户端连接
-    client_socket.close()    
-    server_socket.close()                
+            except ConnectionResetError:
+                #用于处理客户端强制关闭连接的情况 保存文件并关闭系统
+                print("Client forcibly closed the connection")
+                break
+            except socket.timeout:
+                # 超时异常，继续接收 
+                print("time out")
+                # 将超时设置为None,避免超时异常
+                client_socket.settimeout(None)
+                #在重新设置超时的时间
+                #设置接收超时时间为5秒（可根据实际情况调整）
+                client_socket.settimeout(5) 
+                           
+    except Exception as e:
+        print(f"An error occurred: {e}")    
+    finally:
+        #关闭客户端连接和服务器套接字
+        # 关闭客户端连接
+        client_socket.close()    
+        server_socket.close()
+        print("this is successfully closed")                
 
--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 import signal
 import sys
 
-def sig_handler(signum, frame):
-    # 执行自定义操作
-    print("Ctrl+C interrupt detected. Exiting...")
-    # sys.exit()
+# def sig_handler(signum, frame):
+#     # 执行自定义操作
+#     print("Ctrl+C interrupt detected. Exiting...")
+#     sys.exit()
 
-# 注册 Ctrl+C 信号处理程序
-signal.signal(signal.SIGINT, sig_handler)
+# # 注册 Ctrl+C 信号处理程序
+# signal.signal(signal.SIGINT, sig_handler)
 
 # 主程序逻辑
 try:
     while True:
         # 执行你的代码逻辑
+        print("this is running")
         pass
 except KeyboardInterrupt:
     # 捕获 Ctrl+C 中断信号，执行自定义操作
